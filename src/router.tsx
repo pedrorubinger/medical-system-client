73150463597ee418b95dccceeda1cdec919ddf42
Pedro <pedro.rubinger@gmail.com>
2022-02-04 20:33:05 -0300

    fix: fix infinite loop

--- src/router.tsx
+++ src/router.tsx
@@ -37,7 +37,7 @@ export const Router = () => {
     }
 
     setIsMounted(true)
-  }, [dispatch, user?.isAuthorized, user?.validating])
+  }, [])
 
   if (!isMounted || user?.validating) {
     return <ProgressBar />
